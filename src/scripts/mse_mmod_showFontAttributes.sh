d6d4840c57a60785f367ce59c06ca28f0367d57b
Rianna Cantarelli <rianna.aeon@gmail.com>
2022-03-25 01:47:11 -0300

    Ajuste de output

--- src/scripts/mse_mmod_showFontAttributes.sh
+++ src/scripts/mse_mmod_showFontAttributes.sh
@@ -9,15 +9,43 @@
 
 #
 # @desc
-# Mostra os atributos disponíveis no shell
-# que podem ser utilizados para a estilização das mensagens
-# da interface.
+# Mostra os atributos de fonte que podem ser utilizados para a
+# estilização das mensagens da interface.
+#
+# @param string $1
+# Indica o formato que os atributos devem ser apresentados.
+#
+# Use 'list' ou omita este parametro para ver uma lista linha a linha
+# com o nome de cada atributo.
+#
+# Use 'table' para ver as cores em um formato de tabela.
+#
+# @param int $2
+# Indique a quantidade de itens por linha que devem ser mostrados.
+# Se omitido, assumirá o total de 6 itens por linha.
+#
+# @exemple
+#   mse_mmod_showTextColors
 mse_mmod_showFontAttributes() {
 
   local i
+  local mseFormat="list"
   local mseLength=${#MSE_MD_ICOLOR_AVAILABLE_FONT_ATTRIBUTE_NAMES[@]}
   local mseLine
   local mseRawTable
+  local mseLineItens=6
+
+
+  if [ $# -ge 1 ]; then
+    mseFormat="$1"
+  fi
+  if [ $# == 2 ]; then
+    local isInt=$(mse_mmod_checkIfInteger $2)
+    if [ $isInt == 1 ]; then
+      mseLineItens=$2
+    fi
+  fi
+
 
   for (( i=0; i<mseLength; i++)); do
     mseAttrName=${MSE_MD_ICOLOR_AVAILABLE_FONT_ATTRIBUTE_NAMES[$i]}
@@ -25,21 +53,21 @@ mse_mmod_showFontAttributes() {
     if [ "${mseAttrName}" != "DEFAULT" ]; then
 
       mseLine="${mseAttrName}"
-      if (( i % 6 != 0 )); then
-        mseLine+=" : "
-      else
+      if [ "${mseFormat}" == "list" ]; then
         mseLine+="\n"
+      elif [ "${mseFormat}" == "table" ]; then
+        if (( i % mseLineItens != 0 )); then
+          mseLine+=" : "
+        else
+          mseLine+="\n"
+        fi
       fi
 
       mseRawTable+="${mseLine}"
     fi
   done
 
-  printf "\n${lbl_icolor_showFontAttributes_AttrOptions}: \n\n"
   printf "DEFAULT\n"
-
   mseRawTable=$(printf "${mseRawTable}")
   column -e -t -s ":" <<< "${mseRawTable}"
-  printf "\n"
-
 }
