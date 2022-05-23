eb6008977c4c14a51ff9e976aab6b5da8c9504a8
Rianna Cantarelli <rianna.aeon@gmail.com>
2022-05-23 07:12:41 -0300

    check

--- test.sh
+++ test.sh
@@ -25,17 +25,47 @@
 #     done
 #     echo # New line
 # done
+
+
 . src/init.sh
-clear
-ls -la
-ls -la
-mse_inter_setCursorPosition top 10 l
-mse_inter_clearLine
-printf "teste"
-mse_inter_setCursorPosition bottom 1 l
-mse_inter_setCursorPosition right 0 r
-mse_inter_clearLine
-printf "##"
-mse_inter_setCursorPosition bottom 5
-mse_inter_setCursorPosition left 15
-mse_inter_clearLine r
+# clear
+# ls -la
+# ls -la
+# mse_inter_setCursorPosition top 10 l
+# mse_inter_clearLine
+# printf "teste"
+# mse_inter_setCursorPosition bottom 1 l
+# mse_inter_setCursorPosition right 0 r
+# mse_inter_clearLine
+# printf "##"
+# mse_inter_setCursorPosition bottom 5
+# mse_inter_setCursorPosition left 15
+# mse_inter_clearLine r
+
+mse_inter_progressBar "#" "50" "0"
+sleep 1
+mse_inter_progressBar "#" "50" "1"
+sleep 1
+mse_inter_progressBar "#" "50" "2"
+sleep 1
+mse_inter_progressBar "#" "50" "3"
+sleep 1
+mse_inter_progressBar "#" "50" "4"
+sleep 1
+mse_inter_progressBar "#" "50" "5"
+sleep 1
+mse_inter_progressBar "#" "50" "10"
+sleep 1
+mse_inter_progressBar "#" "50" "15"
+sleep 1
+mse_inter_progressBar "#" "50" "20"
+sleep 1
+mse_inter_progressBar "#" "50" "25"
+sleep 1
+mse_inter_progressBar "#" "50" "50"
+sleep 1
+mse_inter_progressBar "#" "50" "75"
+sleep 1
+mse_inter_progressBar "#" "50" "100"
+sleep 1
+
