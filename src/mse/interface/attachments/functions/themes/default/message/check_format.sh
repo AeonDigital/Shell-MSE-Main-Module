feac1b5332073d68b8cc662dbb507277f107b95d
Rianna Cantarelli <rianna@dna.com.br>
2023-07-11 22:27:40 -0300

    doc

--- src/mse/interface/attachments/functions/themes/default/message/check_format.sh
+++ src/mse/interface/attachments/functions/themes/default/message/check_format.sh
@@ -2,22 +2,17 @@
 # myShellEnv v 1.0 [aeondigital.com.br]
 
 
-
-
-
-
-
 #
 # @desc
-# Usado para mensagens.
-# Valida o valor "meta_format" indicado. Sendo válido, retorna ele próprio.
-# Sendo inválido, retorna o valor padrão.
+# Validates the indicated "meta_format" value.
+# If valid, it returns itself.
+# If invalid, returns the default value.
 #
 # @param string $1
-# Valor que está sendo testado
+# Value being tested.
 #
 # @return
-# Valor "meta_format" válido para este tema
+# Valid "meta_format" value for this theme.
 mse_interface_theme_default_message_check_format() {
   declare -a mseAllowedMetaFormat=("custom" "default" "prompt" "status" "title")
 
