193f4779c76fac349f0408ff509796454fc46d0e
Rianna Cantarelli <rianna@dna.com.br>
2023-10-08 00:03:27 -0300

    Adicionada constante 'MSE_ERROR'

--- src/module.sh
+++ src/module.sh
@@ -12,7 +12,7 @@
 # [null] MSE_NULL
 # Used to represent a null value.
 # Value = <<<\0null>>>
-# Check = [ $(printf '%b' '<<<\0null>>>' | wc -c) = 11 ] && echo "s" || echo "n"
+# Check = [[ "${Value}" == "${MSE_NULL}" ]] && echo "yes" || echo "no";
 if [ -z ${MSE_NULL+x} ]; then
   declare -gr MSE_NULL="<<<\0null>>>"
 fi
@@ -21,9 +21,19 @@ fi
 # [undefined] MSE_UNDEF
 # Used to represent a undefined value.
 # Value = <<<\0undef>>>
-# Check = [ $(printf '%b' '<<<\0undef>>>' | wc -c) = 12 ] && echo "s" || echo "n"
+# Check = [[ "${Value}" == "${MSE_UNDEF}" ]] && echo "yes" || echo "no";
 if [ -z ${MSE_UNDEF+x} ]; then
   declare -gr MSE_UNDEF="<<<\0undef>>>"
+fi
+
+
+# [undefined] MSE_ERROR
+# Used to override a value that is considered invalid for a property or function
+# return.
+# Value = <<<\0error>>>
+# Check = [[ "${Value}" == "${MSE_ERROR}" ]] && echo "yes" || echo "no";
+if [ -z ${MSE_ERROR+x} ]; then
+  declare -gr MSE_ERROR="<<<\0error>>>"
 fi
 
 
