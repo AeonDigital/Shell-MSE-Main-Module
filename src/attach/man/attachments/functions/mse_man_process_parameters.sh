834b8f75bd5d17c821b378cc8583fab5fb110363
Rianna Cantarelli <rianna@dna.com.br>
2023-11-06 00:22:45 -0300

    test

--- src/attach/man/attachments/functions/mse_man_process_parameters.sh
+++ src/attach/man/attachments/functions/mse_man_process_parameters.sh
@@ -32,9 +32,10 @@
 # - hint
 # - description
 #
-# Caso alguma propriedade esteja preenchida com algum valor inválido, o mesmo
-# será substituido pela constante '!!MSE_ERROR!!'.
+# Esta função não efetua a validação dos valores indicados no manual
+# portanto os valores podem sim estar definidos de forma errada.
 #
+# @return void
 mse_man_process_parameters() {
   unset MSE_MAN_PARAMETERS_DATA
   declare -Ag MSE_MAN_PARAMETERS_DATA
@@ -43,45 +44,172 @@ mse_man_process_parameters() {
   declare -ag MSE_MAN_PARAMETERS_ORDER=()
 
 
+
   local mseAllowedTypes=" bool int float char charDecimal charHex charOctal string array assoc regex function file dir mixed void "
   declare -a mseArrAllowedTypes=()
   mse_str_convert_toArray "mseArrAllowedTypes" "${mseAllowedTypes}"
 
-  local mseExpectedProperties=" type name aka default min max options list hint description "
+  local mseExpectedProperties=" type name aka default min max options 'options cs' 'options ci' list 'list cl' 'list op' hint description "
   declare -a mseArrExpectedProperties=()
   mse_str_convert_toArray "mseArrExpectedProperties" "${mseExpectedProperties}"
 
 
+
   local mseRawParametersData="${1}"
   if [ "${mseRawParametersData}" != "" ]; then
-    local mseRawDeclaration=""
-    local mseParameterType=""
-    local mseParameterName=""
+    local mseTmpResult=""
     declare -a mseArrSplit=()
 
+    local mseParameterType=""
+    local mseParameterName=""
+    local mseParameterPropertyName=""
+    local mseParameterPropertyValue=""
+    local mseParameterPropertyValueSubList="0"
+    local mseParameterPropertyFirstLine="0"
+
+    local mseAtualOrder="${#MSE_MAN_PARAMETERS_ORDER[@]}"
 
 
-    # @@ daqui!
     IFS=$'\n'
     while read -r mseLineRaw || [ -n "${mseLineRaw}" ]; do
-      if [ "${mseLineRaw}" != "" ]; then
+      if [[ "${mseLineRaw}" == "## "* ]]; then
+        mse_str_split "mseArrSplit" " " "${mseLineRaw##*# }" "1" "1"
+        if [ "${#mseArrSplit[@]}" != "2" ]; then
+          MSE_MAN_PARAMETERS_DATA["ERROR_${mseAtualOrder}"]="${MSE_ERROR}"
+          MSE_MAN_PARAMETERS_ORDER+=("ERROR_${mseAtualOrder}")
+          mseAtualOrder="${#MSE_MAN_PARAMETERS_ORDER[@]}"
 
-        if [[ "${mseLineRaw}" == "## "* ]]; then
-          mseRawDeclaration="${mseLineRaw##*# }"
-          echo "${mseRawDeclaration}"
+          mseParameterType=""
+          mseParameterName=""
+        else
+          mseParameterType="${mseArrSplit[0]}"
+          mseParameterName="${mseArrSplit[1]}"
 
-          mse_str_split "mseArrSplit" " " "${mseRawDeclaration}" "1" "1"
-          if [ "${#mseArrSplit[@]}" != "2" ]; then
-            mseParameterType="${MSE_ERROR}"
-            mseParameterName="ERROR_${#MSE_MAN_PARAMETERS_ORDER[@]}"
-          else
-            mseParameterType="${MSE_ERROR}"
-            mseParameterName="ERROR_${#MSE_MAN_PARAMETERS_ORDER[@]}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}"]="${mseAtualOrder}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_name"]="${mseParameterName}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_type"]="${mseParameterType}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_aka"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_default"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_min"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_max"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_options_ci"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_options_cs"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_list_cl"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_list_op"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_hint"]="${MSE_NULL}"
+          MSE_MAN_PARAMETERS_DATA["${mseParameterName}_description"]="${MSE_NULL}"
+
+          MSE_MAN_PARAMETERS_ORDER+=("${mseParameterName}")
+          mseAtualOrder="${#MSE_MAN_PARAMETERS_ORDER[@]}"
+        fi
+      else
+        if [ "${mseParameterName}" != "" ]; then
+          mseParameterPropertyFirstLine="0"
+
+          if [[ "${mseLineRaw}" == "- "* ]]; then
+            mseParameterPropertyName=""
+            mseParameterPropertyValueSubList="0"
+
+
+            mse_str_split "mseArrSplit" ":" "${mseLineRaw#- }" "0" "1"
+            if [ "${#mseArrSplit[@]}" == "2" ]; then
+              mseTmpResult=$(mse_array_has_value "${mseArrSplit[0]}" "mseArrExpectedProperties" "1")
+              if [ "${mseTmpResult}" == "1" ]; then
+                mseParameterPropertyName="${mseArrSplit[0]}"
+                mseParameterPropertyValue="${mseArrSplit[1]}"
+                mseParameterPropertyFirstLine="1"
+
+
+                if [ "${mseParameterPropertyName}" == "options" ]; then
+                  mseParameterPropertyName="options_ci"
+                fi
+                if [ "${mseParameterPropertyName}" == "list" ]; then
+                  mseParameterPropertyName="list_cl"
+                fi
+
+
+                if [ "${mseParameterPropertyValue}" == "MSE_NULL" ]; then
+                  mseParameterPropertyValue="${MSE_NULL}"
+                elif [ "${mseParameterPropertyValue}" == "MSE_UNDEF" ]; then
+                  mseParameterPropertyValue="${MSE_UNDEF}"
+                fi
+              fi
+            fi
+          fi
+
+
+          if [ "${mseParameterPropertyName}" != "" ]; then
+
+            if [[ "${mseParameterPropertyValueSubList}" == "1" ]]; then
+              if [[ "${mseLineRaw}" == "  - "* ]]; then
+                mseParameterPropertyValue=$(mse_str_trim "${mseLineRaw#  - }")
+              fi
+            fi
+
+
+
+            case "${mseParameterPropertyName}" in
+              "aka")
+                MSE_MAN_PARAMETERS_DATA["${mseParameterName}_aka"]="${mseParameterPropertyValue}"
+                mseParameterPropertyName=""
+              ;;
+              "default")
+                MSE_MAN_PARAMETERS_DATA["${mseParameterName}_default"]="${mseParameterPropertyValue}"
+                mseParameterPropertyName=""
+              ;;
+              "min")
+                MSE_MAN_PARAMETERS_DATA["${mseParameterName}_min"]="${mseParameterPropertyValue}"
+                mseParameterPropertyName=""
+              ;;
+              "max")
+                MSE_MAN_PARAMETERS_DATA["${mseParameterName}_max"]="${mseParameterPropertyValue}"
+                mseParameterPropertyName=""
+              ;;
+              "options_ci" | "options_cs" | "list_cl" | "list_op")
+                mseParameterPropertyValueSubList="1"
+                if [ "${mseParameterPropertyFirstLine}" == "1" ]; then
+                  MSE_MAN_PARAMETERS_DATA["${mseParameterName}_${mseParameterPropertyName}"]=""
+                fi
+
+                if [ "${mseParameterPropertyValue}" != "" ]; then
+                  MSE_MAN_PARAMETERS_DATA["${mseParameterName}_${mseParameterPropertyName}"]+="\n${mseParameterPropertyValue}"
+                fi
+              ;;
+              "hint")
+                if [ "${mseParameterPropertyFirstLine}" == "1" ]; then
+                  MSE_MAN_PARAMETERS_DATA["${mseParameterName}_hint"]=""
+                else
+                  mseTmpResult=$(mse_str_trim "${mseLineRaw}")
+                  if [ "${mseTmpResult}" == "" ]; then
+                    mseParameterPropertyName="description"
+                    MSE_MAN_PARAMETERS_DATA["${mseParameterName}_description"]=""
+                  else
+                    MSE_MAN_PARAMETERS_DATA["${mseParameterName}_hint"]+="\n${mseTmpResult}"
+                  fi
+                fi
+              ;;
+              "description")
+                mseTmpResult=$(mse_str_trim "${mseLineRaw}")
+                if [ "${MSE_MAN_PARAMETERS_DATA["${mseParameterName}_description"]}" != "" ] || [ "${mseTmpResult}" != "" ]; then
+                  MSE_MAN_PARAMETERS_DATA["${mseParameterName}_description"]+="\n${mseTmpResult}"
+                fi
+              ;;
+            esac
           fi
         fi
-
       fi
     done <<< "${mseRawParametersData}"
     IFS=$' \t\n'
+
+
+    local mseV
+    for mseV in "${MSE_MAN_PARAMETERS_ORDER[@]}"; do
+      if [ "${MSE_MAN_PARAMETERS_DATA["${mseV}_hint"]}" != "" ]; then
+        MSE_MAN_PARAMETERS_DATA["${mseV}_hint"]=$(mse_str_trim "${MSE_MAN_PARAMETERS_DATA["${mseV}_hint"]}")
+      fi
+      if [ "${MSE_MAN_PARAMETERS_DATA["${mseV}_hint"]}" != "" ]; then
+        MSE_MAN_PARAMETERS_DATA["${mseV}_description"]=$(mse_str_trim "${MSE_MAN_PARAMETERS_DATA["${mseV}_description"]}")
+      fi
+    done
   fi
 }
--- src/attach/man/test.sh
+++ src/attach/man/test.sh
@@ -8,20 +8,23 @@ test_mse_man() {
 
 
   # 01
-  test_mse_man_reset_data
+  #test_mse_man_reset_data
 
   # 02
-  test_mse_man_normalize_section_name
+  #test_mse_man_normalize_section_name
 
   # 03
-  test_mse_man_extract_main_sections_data
+  #test_mse_man_extract_main_sections_data
 
   # 04
-  test_mse_man_process_section_generic
+  #test_mse_man_process_section_generic
+
+  # 05
+  test_mse_man_process_parameters
 
 
   if [ 1 == 2 ]; then
-    echo "SEGUIR DAQUI!!!"
+    echo "SEGUIR DAQUI PROCESSANDO OS PARAMETROS!!!"
     # - mse_man_process_parameters
     # - mse_man_process_section_data
 
@@ -248,4 +251,97 @@ test_mse_man_process_section_generic() {
 
     mse_utest_assert_string_multiline
   done
+}
+
+
+
+test_mse_man_process_parameters() {
+  mse_man_reset_data
+  local dir=$(echo "${BASH_SOURCE%/*}")
+
+  mse_man_extract_main_sections_data "${dir}/attachments/test/man/pt-br.md" ". 'Extra section'"
+
+  testResult="${#MSE_MAN_MAIN_SECTIONS_ORDER[@]}"
+  testExpected="7"
+
+  mse_utest_assert_equals
+
+
+
+
+
+  mse_man_process_section_generic "MSE_MAN_MAIN_SECTIONS_DATA" "parameters" "1"
+
+  testResult="${#MSE_MAN_GENERIC_SECTION_DATA[@]}"
+  testExpected="4"
+
+  mse_utest_assert_equals
+
+
+
+
+
+  testResult="${MSE_MAN_GENERIC_SECTION_DATA[subsections]}"
+  testExpected=$(< "${dir}/attachments/test/expected/process_section_generic/parameters_subsections.txt")
+
+  mse_utest_assert_string_multiline
+
+
+
+
+
+  #
+  # Processa os dados dos parametros
+  mse_man_process_parameters "${MSE_MAN_GENERIC_SECTION_DATA[subsections]}"
+
+  testResult="${#MSE_MAN_PARAMETERS_ORDER[@]}"
+  testExpected="6"
+
+  mse_utest_assert_equals
+
+
+
+  testExpected="1"
+  local mseParameterName
+  for mseParameterName in "${MSE_MAN_PARAMETERS_ORDER[@]}"; do
+    testResult="0"
+    if [ ! -z "${MSE_MAN_PARAMETERS_DATA[$mseParameterName]+x}" ]; then
+      testResult="1"
+    fi
+    mse_utest_assert_equals
+  done
+
+
+
+
+  #
+  # Testa os valores obtidos
+  declare -A mseAssocTest
+  mseAssocTest["name"]="alpha"
+  mseAssocTest["type"]="!bool"
+  mseAssocTest["aka"]="-a --alpha"
+  mseAssocTest["default"]="${MSE_NULL}"
+  mseAssocTest["min"]="${MSE_NULL}"
+  mseAssocTest["max"]="${MSE_NULL}"
+  mseAssocTest["options_ci"]="${MSE_NULL}"
+  mseAssocTest["options_cs"]="${MSE_NULL}"
+  mseAssocTest["list_cl"]="${MSE_NULL}"
+  mseAssocTest["list_op"]="${MSE_NULL}"
+  mseAssocTest["hint"]="Definição simples de um parametro booleano que também é \`nullable\`."
+  mseAssocTest["description"]=""
+
+  local mseK
+  local msePName="${mseAssocTest["name"]}"
+  local msePNameTest=""
+  for mseK in "${!mseAssocTest[@]}"; do
+    msePNameTest="${msePName}_${mseK}"
+    testResult="${MSE_MAN_PARAMETERS_DATA[${msePNameTest}]}"
+    testExpected="${mseAssocTest[${mseK}]}"
+
+    mse_utest_assert_equals
+  done
+
+
+  # SEGUIR DAQUI COM O TESTE DAS DEMAIS PROPRIEDADES!
+
 }
