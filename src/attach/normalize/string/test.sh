d75c0b95cc68a98465d46959bc8b96f4008c7db0
Rianna Cantarelli <rianna@dna.com.br>
2023-11-08 23:43:29 -0300

    normalize

---
+++ src/attach/normalize/string/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# myShellEnv v 1.0 [aeondigital.com.br]
+
+
+test_mse_normalize_string() {
+  local mseTestMultilineResult=""
+  local mseTestMultilineExpected=""
+
+  mseTestMultilineResult+="\ntest 0 with \`multiline\`"
+  mseTestMultilineResult+="\nstring "
+  mseTestMultilineExpected+="\ntest 0 with \`multiline\`"
+  mseTestMultilineExpected+="\nstring "
+
+
+  testResult="${mseTestMultilineResult}"
+  testExpected="${mseTestMultilineExpected}"
+
+  mse_utest_assert_equals
+
+
+
+  testResult=$(mse_normalize_string "${mseTestMultilineResult}")
+  testExpected="${mseTestMultilineExpected}"
+
+  mse_utest_assert_not_equals
+}
