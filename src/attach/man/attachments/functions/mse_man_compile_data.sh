81110fddc9bb06df5294149dbe0842309e8cc49b
Rianna Cantarelli <rianna@dna.com.br>
2023-11-10 00:28:20 -0300

    man

--- src/attach/man/attachments/functions/mse_man_compile_data.sh
+++ src/attach/man/attachments/functions/mse_man_compile_data.sh
@@ -16,27 +16,32 @@ mse_man_compile_data() {
   mse_man_extract_sections_data "${mseTargetFile}"
 
 
+  declare -a mseExpectedSectionSubParts=("title" "summary" "description" "subsections");
+  declare -a mseExpectedSectionParameterProperties=("name" "type" "aka" "default" "min" "max" "options_ci" "options_cs" "list_cl" "list_op" "hint" "description");
+
   local mseSectionName
   local mseSectionSubPart
   local mseSectionParameterName
+  local mseSectionParameterPropertyName
 
   local mseNewKey
   for mseSectionName in "${MSE_MAN_MAIN_SECTIONS_ORDER[@]}"; do
     mse_man_process_section_data "${mseSectionName}" "1"
 
-    for mseSectionSubPart in title summary description subsections; do
+    for mseSectionSubPart in "${mseExpectedSectionSubParts[@]}"; do
       mseNewKey="${mseSectionName}_${mseSectionSubPart}"
       mseInternalAssocCompileManName["${mseNewKey}"]="${MSE_MAN_SECTION_DATA[${mseSectionSubPart}]}"
       mseInternalArrCompileManOrder+=("${mseNewKey}")
 
       if [ "${mseSectionName}" == "parameters" ] && [ "${mseSectionSubPart}" == "subsections" ] && [ "${MSE_MAN_SECTION_DATA["subsections"]}" != "" ]; then
         mse_man_process_parameters "${MSE_MAN_SECTION_DATA[subsections]}"
-
         for mseSectionParameterName in "${MSE_MAN_PARAMETERS_ORDER[@]}"; do
-          mseNewKey="${mseSectionName}_${mseSectionParameterName}"
-          mseInternalAssocCompileManName["${mseNewKey}"]="${MSE_MAN_PARAMETERS_ORDER[${mseSectionParameterName}]}"
-
-          mseInternalArrCompileManOrder+=("${mseNewKey}")
+          for mseSectionParameterPropertyName in "${mseExpectedSectionParameterProperties[@]}" ; do
+            mseNewKey="${mseSectionName}_${mseSectionParameterName}_${mseSectionParameterPropertyName}"
+            mseNewKey="${mseNewKey,,}"
+            mseInternalAssocCompileManName["${mseNewKey}"]="${MSE_MAN_PARAMETERS_DATA["${mseSectionParameterName}_${mseSectionParameterPropertyName}"]}"
+            mseInternalArrCompileManOrder+=("${mseNewKey}")
+          done
         done
       fi
     done
